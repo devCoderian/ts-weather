568297b08644e1b26150db8811f4261403a38641
devCoderian <devcoderian@gmail.com>
2021-10-28 23:26:01 +0900

    useState ts example

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,35 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
-function App() {
+interface State{
+  userName: string;
+  imageUrl: string;
+}
+
+const App:React.FC = () => {
+  // 일반적으로 훅을 사용하면 클래스를 작성하거나 상속없이 컴포넌트에 동작을 추가할 수 있다.
+  // 함수 컴포넌트에 "함수를 유지하면서 기능을 추가하고 싶다."
+  
+  // 훅은 use라는 단어로 시작한다.
+  // 바벨은 use를 감지해 일반 함수와 훅을 구별한다.
+  // useState -> 컴포넌트 상태 관리 -> 현재 상태 값과 업데이트할 함수를 반환한다. 
+  // useEffect -> 부가동작 (서버에서 데이터를 가져올 때)
+
+  // const [userName, setUserName] = useState('IAN');
+  // const [imageUrl, setImageUrl] = useState('https://picsum.photos/600/150');
+
+  const [state, setState] = useState<State>({
+    userName: 'IAN',
+    imageUrl: 'https://picsum.photos/600/150'
+  });
+
+  const MyStyles = {margin: 40};
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div style = {MyStyles}>
+    <h1>{state.userName}</h1>
+    <img src = {state.imageUrl} alt ="" />
+   </div>
   );
 }
 
